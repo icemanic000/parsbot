79af36b636cabcef68878eb5b57e477aa0b90c8d
Serhii <80172305+icemanic000@users.noreply.github.com>
2025-05-30 01:38:42 +0300

    Update bot.py 1

--- bot.py
+++ bot.py
@@ -50,3 +50,4 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
+ 
